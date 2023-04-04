bceacbabac37a89692c1b3978dc3c10fd6ea1195
MasterOogway6147 <125769645+MasterOogway6147@users.noreply.github.com>
2023-04-04 15:01:36 +0200

    Update Death Farm.lua

--- Doors/Death Farm.lua
+++ Doors/Death Farm.lua
@@ -2,5 +2,5 @@ if game.PlaceId == 6839171747 then
     repeat task.wait() until game:IsLoaded() and game.Players and game.Players.LocalPlayer and game.Players.LocalPlayer:FindFirstChild("PlayerGui") and game.Players.LocalPlayer:FindFirstChild("PlayerGui"):FindFirstChild("MainUI") and game.Players.LocalPlayer:FindFirstChild("PlayerGui"):FindFirstChild("MainUI"):FindFirstChild("ItemShop") and game.Players.LocalPlayer:FindFirstChild("PlayerGui"):FindFirstChild("MainUI"):FindFirstChild("ItemShop").Visible == true task.wait(1)
     game.Players.LocalPlayer.Character.Humanoid.Health = 0
     game.ReplicatedStorage.EntityInfo.PlayAgain:FireServer()
-    (queue_on_teleport or (syn and syn.queue_on_teleport))(game:HttpGet(""))
+    (queue_on_teleport or (syn and syn.queue_on_teleport))(game:HttpGet("https://raw.githubusercontent.com/MasterOogway6147/Scripts/main/Doors/Death%20Farm.lua"))
 end
