a97f630fe897ccc7aae977a835265368b178c8aa
MasterOogway6147 <125769645+MasterOogway6147@users.noreply.github.com>
2023-04-04 15:00:42 +0200

    Create Death Farm.lua

---
+++ Doors/Death Farm.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 6839171747 then
+    repeat task.wait() until game:IsLoaded() and game.Players and game.Players.LocalPlayer and game.Players.LocalPlayer:FindFirstChild("PlayerGui") and game.Players.LocalPlayer:FindFirstChild("PlayerGui"):FindFirstChild("MainUI") and game.Players.LocalPlayer:FindFirstChild("PlayerGui"):FindFirstChild("MainUI"):FindFirstChild("ItemShop") and game.Players.LocalPlayer:FindFirstChild("PlayerGui"):FindFirstChild("MainUI"):FindFirstChild("ItemShop").Visible == true task.wait(1)
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
+    game.ReplicatedStorage.EntityInfo.PlayAgain:FireServer()
+    (queue_on_teleport or (syn and syn.queue_on_teleport))(game:HttpGet(""))
+end
