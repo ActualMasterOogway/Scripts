d8fe91d78a618cfcdc5b8a52ef207d0c78b58e48
MasterOogway6147 <125769645+MasterOogway6147@users.noreply.github.com>
2023-05-01 11:35:40 +0200

    Update Auto Hop EDITED.lua

--- Star Scape/Auto Hop EDITED.lua
+++ Star Scape/Auto Hop EDITED.lua
@@ -9,7 +9,7 @@ end
 local queue_on_tp = (syn and syn.queue_on_teleport or queue_on_teleport)
 local secure_call = (syn and syn.secure_call or cus_secure_call)
 game:GetService("ReplicatedStorage"):WaitForChild("Remote", 60):WaitForChild("Effects", 60):WaitForChild("ExitWarpEffect", 60)
-task.wait(3)
+task.wait(3.5)
 local Routes = require(game:GetService("ReplicatedStorage"):WaitForChild("Source", 60):WaitForChild("Client", 60):WaitForChild("Helpers", 60):WaitForChild("Routes", 60))
 local go = false
 local NM NM = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
