57a6a8c7ab992255b8b3e773b0d667620959fa2d
MasterOogway6147 <125769645+MasterOogway6147@users.noreply.github.com>
2023-05-01 11:37:59 +0200

    Update Auto Hop EDITED.lua

--- Star Scape/Auto Hop EDITED.lua
+++ Star Scape/Auto Hop EDITED.lua
@@ -10,6 +10,7 @@ local queue_on_tp = (syn and syn.queue_on_teleport or queue_on_teleport)
 local secure_call = (syn and syn.secure_call or cus_secure_call)
 game:GetService("ReplicatedStorage"):WaitForChild("Remote", 60):WaitForChild("Effects", 60):WaitForChild("ExitWarpEffect", 60)
 task.wait(3.5)
+print("loading routes and namecall")
 local Routes = require(game:GetService("ReplicatedStorage"):WaitForChild("Source", 60):WaitForChild("Client", 60):WaitForChild("Helpers", 60):WaitForChild("Routes", 60))
 local go = false
 local NM NM = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
@@ -18,7 +19,9 @@ local NM NM = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
 	end
 	return NM(self, ...)
 end))
+print("wating for go")
 repeat task.wait() until go == true
+print("go is true")
 task.wait(3.34)
 queue_on_tp(game:HttpGet("https://raw.githubusercontent.com/MasterOogway6147/Scripts/main/Star%20Scape/Auto%20Hop%20EDITED.lua", true))
 secure_call(require(game:GetService("ReplicatedStorage").Source.Client.Flight.Warp).leaveSystem, game:GetService("Players").LocalPlayer:WaitForChild("PlayerScripts"):WaitForChild("Game"), game:GetService("ReplicatedStorage").System.Neighbors[Routes.getNextSystemName()].Value)
