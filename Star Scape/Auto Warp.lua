6f0c48494382157f4cb8625f16c6fe256a235b13
MasterOogway6147 <125769645+MasterOogway6147@users.noreply.github.com>
2023-05-01 17:19:21 +0200

    Update Auto Warp.lua

--- Star Scape/Auto Warp.lua
+++ Star Scape/Auto Warp.lua
@@ -11,6 +11,49 @@ local secure_call = (syn and syn.secure_call or cus_secure_call)
 game:GetService("ReplicatedStorage"):WaitForChild("Remote", 60):WaitForChild("Effects", 60):WaitForChild("ExitWarpEffect", 60)
 task.wait(3.5)
 local Routes = require(game:GetService("ReplicatedStorage"):WaitForChild("Source", 60):WaitForChild("Client", 60):WaitForChild("Helpers", 60):WaitForChild("Routes", 60))
+function TPShip(Position)
+    local M = Instance.new("Model")
+    local Obj = Instance.new("Part", M)
+    Obj.Name = "Object"
+    local St = Instance.new("Part", M)
+    St.Name = "Stats"
+    local Z = Instance.new("NumberValue", St)
+    Z.Name = "Zone"
+    local T = Instance.new("NumberValue", St)
+    T.Name = "Type"
+    task.spawn(function()
+        require(game:GetService("ReplicatedStorage").Source.Client.Flight.Warp).warpToTarget(M)
+    end)
+    task.wait(.5)
+    if typeof(Position) == "Vector3" then
+        Ship:SetPrimaryPartCFrame(CFrame.new(Position))
+    elseif typeof(Position) == "CFrame" then
+        Ship:SetPrimaryPartCFrame(Position)
+    end
+    task.wait(.5)
+    game:GetService("VirtualInputManager"):SendKeyEvent(true, 0x78, false, game)
+	task.wait()
+	game:GetService("VirtualInputManager"):SendKeyEvent(false, 0x78, false, game)
+end
+function MouseClick(Type, XPos, YPos)
+    if typeof(XPos) ~= "number" then
+        return error("Expected Number at X Position (got "..typeof(XPos)..")")
+    elseif typeof(YPos) ~= "number" then
+        return error("Expected Number at Y Position (g  ot "..typeof(YPos)..")")
+    end
+    if string.match(Type:lower(), "left") then
+        game:GetService("VirtualInputManager"):SendMouseButtonEvent(XPos, YPos, 0, true, game, 0) task.wait() game:GetService("VirtualInputManager"):SendMouseButtonEvent(XPos, YPos, 0, false, game, 0)
+    elseif string.match(Type:lower(), "right") then
+        game:GetService("VirtualInputManager"):SendMouseButtonEvent(XPos, YPos, 1, true, game, 0) task.wait() game:GetService("VirtualInputManager"):SendMouseButtonEvent(XPos, YPos, 1, false, game, 0)
+    else
+        return error("Expected Mouse Click Left or Right (got "..Type..")")
+    end
+end
+for i,v in pairs(Ship:GetDescendants()) do
+    if v:IsA("BasePart") and v.CanCollide then
+        v.CanCollide = false
+    end
+end
 local go = false
 local NM NM = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
 	if not checkcaller() and getnamecallmethod() == "FireServer" and self.Name == "ExitWarpEffect" then
