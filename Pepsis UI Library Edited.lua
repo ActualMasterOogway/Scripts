7429b44fc794a661c38ae45dd415bd39bd74da9b
Master Oogway <125769645+ActualMasterOogway@users.noreply.github.com>
2024-03-13 14:27:38 +0100

    Update Pepsis UI Library Edited.lua

--- Pepsis UI Library Edited.lua
+++ Pepsis UI Library Edited.lua
@@ -1,2 +1,2 @@
-warn("Please tell the owner of the Script to update the Library Repo to 'https://github.com/ActualMasterOogway/Pepsi-UI-Library/blob/main/Pepsi-UI-Library.lua'")
+warn("Please tell the owner of the Script to update the Library Repo to 'https://github.com/ActualMasterOogway/Pepsi-UI-Library/blob/main/Pepsi-UI-Library.lua' for faster loading times")
 return loadstring(game:HttpGet("https://github.com/ActualMasterOogway/Pepsi-UI-Library/blob/main/Pepsi-UI-Library.lua"))()
