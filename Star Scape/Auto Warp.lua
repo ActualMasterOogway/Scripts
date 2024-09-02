f53aef018fbfcb4cf0e9b555997035f49dcd6222
Master Oogway <125769645+ActualMasterOogway@users.noreply.github.com>
2024-09-02 17:50:55 +0200

    Update Auto Warp.lua

--- Star Scape/Auto Warp.lua
+++ Star Scape/Auto Warp.lua
@@ -2,23 +2,24 @@ if not game:IsLoaded() then
 	game.Loaded:Wait()
 end
 
-if not syn then
+if not syn or not syn.secure_call then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/Lua_SecureCall", true))()
 end
 
 local queue_on_tp = (syn and syn.queue_on_teleport or queue_on_teleport)
 local secure_call = ((syn and syn.secure_call) or cus_secure_call)
-local go = false
-local NM NM = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
-	if not checkcaller() and getnamecallmethod() == "FireServer" and self.Name == "ExitWarpEffect" then
-		go = true
-	end
-	return NM(self, ...)
-end))
-repeat task.wait() until go == true
-task.wait(2.35)
+
+local function GetSectorByName(Name: string)
+    for i,v in next, game:GetService("ReplicatedStorage").Galaxy.Sectors:GetDescendants() do
+        if v.Name == Name then
+            return v
+        end
+    end
+end
+
 local Routes = require(game:GetService("ReplicatedStorage"):WaitForChild("Source", 60):WaitForChild("Client", 60):WaitForChild("Helpers", 60):WaitForChild("Routes", 60))
 if Routes.getNextSystemName() ~= nil then
 	queue_on_tp(game:HttpGet("https://raw.githubusercontent.com/MasterOogway6147/Scripts/main/Star%20Scape/Auto%20Warp.lua", true))
-	secure_call(require(game:GetService("ReplicatedStorage").Source.Client.Flight.Warp).leaveSystem, game:GetService("Players").LocalPlayer:WaitForChild("PlayerScripts"):WaitForChild("Game"), game:GetService("ReplicatedStorage").System.Neighbors[Routes.getNextSystemName()].Value)
+
+    secure_call(require(game:GetService("ReplicatedStorage").Source.Client.Actions.WarpToStar), game:GetService("Players").LocalPlayer:WaitForChild("PlayerScripts"):WaitForChild("Game"), GetSectorByName(Routes.getNextSystemName()))
 end
